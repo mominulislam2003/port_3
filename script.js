8dde44f43b03ea940592a1dc0dbed2d3381fa357
Mominul Islam <mominulislam.miad@gmail.com>
2026-02-27 12:02:44 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     rocket.classList.add("launching");
                     submitBtn.innerText = "Sent into Orbit!";
                     setTimeout(() => {
-                        status.innerText = "Message is send successfully!";
+                        status.innerText = "Message reached the stars!";
                         rocket.classList.remove("launching");
                         submitBtn.innerText = "Send Message";
                         form.reset();
@@ -238,5 +238,4 @@ document.addEventListener('contextmenu', e => {
 });
 document.addEventListener('keydown', e => {
     if (e.ctrlKey && (e.key === 's' || e.key === 'u') || e.key === 'F12') e.preventDefault();
-
 });
